32c5322af9bdc05eda9768f373fab02393c727c6
Rahul Kumar <rahulkumar28@outlook.co.nz>
2015-08-31 15:10:11 +1200

    Fixed bug where a file written by FISS could not be read. May need to to efficiency improvements next

--- src/Sample/Bottle/runkeeper_demo.py
+++ src/Sample/Bottle/runkeeper_demo.py
@@ -100,9 +100,6 @@ def welcome():
         points = HealthGraphPackage.Points.Points(act_iter,strength_act_iter,weight_iter)
         write_to_file(user, points)
         
-#         total_points = points.get_total_points()
-#         print("Total points for the past week was: " + str(total_points))
-
         activities = [act_iter.next() for _ in range(1)] 
         return bottle.template('welcome.html', 
                                profile=profile,
@@ -125,20 +122,40 @@ def write_to_file(userToken, points):
     file_to_write = []
 
     if os.path.isfile(path_to_FISS + file_name):
+        print("OS PATH FILE IS TRUE")
         read_file = open(path_to_FISS + file_name)
         for i, line in enumerate(read_file):
+            if i == 0:
+                try:
+                    first_date_index = line.index("<First_import_date>")
+                    first_date_in_format = line[first_date_index + 20:first_date_index + 24] + line[first_date_index + 26:first_date_index + 28]
+                    print("i ==0 " + first_date_in_format)
+                    first_date = datetime.strptime(first_date_in_format, "%d%m%y")
+                    first_import_date = "" +  str("%02d" %first_date.day) + str("%02d" %first_date.month) + str(first_date.year)
+
+                    previous_import_date_index = line.index("<Last_update_data>")
+                    previous_import_date = line[previous_import_date_index + 19: previous_import_date_index + 27]
+                    print("PREVIOUS IMPROT DATE " + previous_import_date)
+                except ValueError:
+                    print("File being read is from python not Skyrim")
             if i == 3:
                 first_date = line
                 first_date_in_format = first_date[20:24] + first_date[26:28]
+                if first_date_in_format == " </Fst":
+                    raise Exception("Please enter the first date the game was synced")
                 first_date = datetime.strptime(first_date_in_format, "%d%m%y")
                 first_import_date = "" +  str("%02d" %first_date.day) + str("%02d" %first_date.month) + str(first_date.year)
+                print("FIRST IMPORT DATE = " + first_import_date)
             if i == 4:
-                first_weeks_points_str = line
-                index = first_weeks_points_str.index("</First week points>") - 1
-                first_weeks_points_str = line[20:index]
-                first_weeks_points = int(first_weeks_points_str)
+                # first_weeks_points_str = line
+                # print("FIRST WEEK POINTS " + str(first_weeks_points))
+                # index = first_weeks_points_str.index("</First week points>") - 1
+                # first_weeks_points_str = line[20:index]
+                # first_weeks_points = int(first_weeks_points_str)
+                pass
             if i == 5:
                 previous_import_date = line
+                print("PREVIOUS IMPROT DATE " + previous_import_date)
                 previous_import_date = previous_import_date[19:27]
         read_file.close()
     else:
@@ -149,6 +166,7 @@ def write_to_file(userToken, points):
 #     Else use what is in the file
     previous_import_date_object = date(date.today().year, date.today().month, 1)
     if previous_import_date != "":
+        print("Previous import date set as object")
         previous_import_date_object = datetime.strptime(previous_import_date, "%d%m%Y").date()
         
     file_to_write.append("<fiss><Header><Version>1.2</Version><ModName>P4P</ModName></Header>\n<Data>\n\n")
@@ -157,6 +175,7 @@ def write_to_file(userToken, points):
     file_to_write.append("<Last_update_data> " + todays_date + " </Last_update_data> \n\n")
 
     one_week_from_first_import = (first_date + timedelta(days = 7)).date()
+    print("one wekk = " + str(one_week_from_first_import))
     first_week_completed = datetime.now().date() > one_week_from_first_import
     file_to_write.append("<First_week_completed> " + str(first_week_completed) + " </First_week_completed> \n\n")
     
@@ -167,13 +186,15 @@ def write_to_file(userToken, points):
     sport_exercises = []
     for exercise in fitness_act_iter:
         exercise_date = exercise.get("start_time").date()
-        
+        print(str(exercise_date))
+        if (exercise_date >= first_date.date()) and (exercise_date <= one_week_from_first_import):
+            exercise_points = points.get_points(exercise)
+            first_weeks_points += exercise_points
+            print("Added to first_weeks_points")
+
         if previous_import_date_object <= exercise_date:
             exercise_type = HealthGraphPackage.Points.get_exer_name(exercise)
-                
             exercise_points = points.get_points(exercise)
-            if exercise_date <= one_week_from_first_import:
-                first_weeks_points += exercise_points    
                 
             if HealthGraphPackage.Points.get_type(exercise) != 'Other':
                 fitness_exercise = "<fitness_exercise " + str(index) + "> "
@@ -190,11 +211,8 @@ def write_to_file(userToken, points):
     index = 1
     for exercise in sport_exercises:
         exercise_type = HealthGraphPackage.Points.get_exer_name(exercise)
-        
         exercise_points = points.get_points(exercise)
-        if exercise_date <= one_week_from_first_import:
-            first_weeks_points += exercise_points
-
+        
         sport_exercise = "<sport_exercise " + str(index) + "> "
         sport_exercise += "<type> " + exercise_type + " </type> "
         sport_exercise += "<points> " + str(exercise_points) + " </points> "
@@ -208,13 +226,14 @@ def write_to_file(userToken, points):
     strength_act_iter = userToken.get_strength_activity_iter()
     for exercise in strength_act_iter:
         exercise_date = exercise.get("start_time").date()
-        
+
+        if (exercise_date >= first_date.date()) and (exercise_date <= one_week_from_first_import):
+            exercise_points = points.get_points(exercise)
+            first_weeks_points += exercise_points
+
         if previous_import_date_object <= exercise_date:
             exercise_type = HealthGraphPackage.Points.get_exer_name(exercise)
-            
             exercise_points = points.get_points(exercise)
-            if exercise_date <= one_week_from_first_import:
-                first_weeks_points += exercise_points
             
             strength_exercise = "<strength_exercise " + str(index) + "> "
             strength_exercise += "<type> " + exercise_type + " </type> "
