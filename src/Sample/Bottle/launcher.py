425cc06a06566e2a0b1c50be9e5ea96924aa5c83
Rahul Kumar <rahulkumar28@outlook.co.nz>
2015-09-15 18:37:53 +1200

    Added new launcher

---
+++ src/Sample/Bottle/launcher.py
@@ -0,0 +1,31 @@
+import webbrowser
+import threading
+import os
+new = 2 # open in a new tab, if possible
+
+# open a public URL, in this case, the webbrowser docs
+
+url = "127.0.0.1:8000"
+
+class bottleThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        os.system("python runkeeper_demo.py")
+
+class browserThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        webbrowser.open(url,new=new)
+
+
+
+if __name__ == "__main__":
+    thread1 = bottleThread()
+    thread2 = browserThread()
+    
+    thread1.start() 
+    thread2.start()
+    
+    print"exiting"
